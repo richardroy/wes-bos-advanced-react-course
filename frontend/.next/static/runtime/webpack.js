4c0e3b09702c52399680f6b68f765474c61c9323
richardroy <richardroygibson@gmail.com>
2018-10-21 10:28:55 +1100

    added the signup page and component, user can now singup via the frontend

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e1424e71d266ed7801cf";
+/******/ 	var hotCurrentHash = "7b732fe064e69ce5d136";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

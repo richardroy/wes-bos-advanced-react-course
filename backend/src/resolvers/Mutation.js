c58ff35873a2a9322f00315e7b694afbbf11ed5d
richardroy <richardroygibson@gmail.com>
2018-11-08 21:40:36 +1100

    added backend code for reseting a password

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const  { randomBytes } = require('crypto');
+const { promisify } = require('util');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -78,7 +80,56 @@ const Mutations = {
     return {
       message: 'Log out successful.'
     }
-  }
+  },
+
+  async requestReset(parent, args, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+    if(!user) {
+      throw new Error(`No such user found for email ${args.email}`);
+    }
+    const randomBytesPromisified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisified(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000;
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry }
+    });
+    console.log(res);
+    return { message: 'Thanks' };
+
+
+  },
+
+  resetPassword(parent, args, ctx, info) {
+    if(args.password !== args.confirmPassword) {
+      throw new Error('Yo Passwords dont match!');
+    }
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 360000
+      }
+    });
+    if(!user) {
+      throw new Error('This token is either invalid or expired!');
+    }
+
+    const password = await bycrypt.hash(args.password, 10);
+    const updatedUser = await ctx.db.mutation.updatedUser({
+      where: {email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null
+      }
+    });
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    return updatedUser
+  },
 };
 
 module.exports = Mutations;
